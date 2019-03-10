5169adee787b0ecaea38c71df8759e8ee5467905
jainvikash <43354376+jainvikash@users.noreply.github.com>
2019-03-10 03:21:04 -0700

    Add files via upload

---
+++ react/redux/precache-manifest.9dd0e0c781442d7e2b9d6d4923a1f476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a47f8bc1fbc072aefec0",
+    "url": "/static/js/main.a47f8bc1.chunk.js"
+  },
+  {
+    "revision": "dec01d80bb7e804e447a",
+    "url": "/static/js/2.dec01d80.chunk.js"
+  },
+  {
+    "revision": "a47f8bc1fbc072aefec0",
+    "url": "/static/css/main.5c7a83b9.chunk.css"
+  },
+  {
+    "revision": "85ac2ea2abada36c30bf31721ed05362",
+    "url": "/index.html"
+  }
+];
