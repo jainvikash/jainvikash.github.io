e3fe9f247cfa8c88bfee14b2b13a801cf1597457
jainvikash <43354376+jainvikash@users.noreply.github.com>
2019-07-07 12:25:27 +0530

    movie api supportive files

---
+++ react/api/movie/precache-manifest.5e19695a91a5e915f2d98733a2141f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5753d593f7846a108473ed5c1a2067b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e2fbf078b2cfa079986",
+    "url": "/static/css/main.e3982a7a.chunk.css"
+  },
+  {
+    "revision": "c968c3c860871433d5c7",
+    "url": "/static/js/2.a2754bdc.chunk.js"
+  },
+  {
+    "revision": "5e2fbf078b2cfa079986",
+    "url": "/static/js/main.2d06ffd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
